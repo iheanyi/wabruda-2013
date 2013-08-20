5a9e222730e63bb6cfd6decf0fba01bb1a591c9f
Iheanyi Ekechukwu <iekechukwu@gmail.com>
2013-08-20 17:58:21 -0500

    Navigation element changing the game

--- js/main.js
+++ js/main.js
@@ -2,10 +2,19 @@ $(document).ready(function() {
 	$.stellar();
 
 	console.log("Loading Nav.");
-	//$('#nav').onePageNav();
+	$('#nav').onePageNav();
 
-	console.log("LOADED");;
-	$('.nav a').on('click', function(){
+	console.log("LOADED");
+
+	$('li').click(function() {
+		$("li.active").removeClass("active");
+		$(this).addClass('active');
+	});
+
+	console.log("Now we're just checking something else...");
+	
+	$('.nav a').on('click', function() {
+
 		if ($('.navbar-toggle').is(":visible")) {
 			$('.navbar-toggle').click();
 		}
