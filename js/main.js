817cb29cb2ee0966cff36567157fdd7a0adfc5c1
Iheanyi Ekechukwu <iekechukwu@gmail.com>
2013-08-20 15:10:40 -0500

    Blurred background, right align navigation, starting CSS overhaul.

--- js/main.js
+++ js/main.js
@@ -1,3 +1,12 @@
+$.stellar();
+
+$('.top-slide').blurjs({
+    source: 'body',     //Background to blur
+    radius: 5,          //Blur Radius
+    overlay: 'rgba(120,255,255,0.4)'
+        //Overlay Color, follow CSS3's rgba() syntax
+});
+
 $('.nav a').on('click', function(){
 	if ($('.navbar-toggle').is(":visible")) {
 		$('.navbar-toggle').click();
