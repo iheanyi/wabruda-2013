14ba6dd1c3e55a9ab7a3867a9892d41b7fd3ede7
Iheanyi Ekechukwu <iekechukwu@gmail.com>
2013-08-20 21:09:09 -0500

    JavaScript changes. CSS changes. New navbar.

--- js/main.js
+++ js/main.js
@@ -1,7 +1,42 @@
 $(document).ready(function() {
 	$.stellar();
 
+	var scrollTimeout = null;
+	onScroll();
+	onScrollEnd();
+
+	var panels = $('.slide');
+	var links =  $('#nav li');
+
+	var secondLink = $(links[1]).children("a:first").attr('href');
+	console.log(panels.length);
+	console.log($(links[1]).children("a:first").attr('href'));
+
+	console.log($(secondLink).scrollTop());
+	var firstTop = $(panels[0]).offset().top;
+	var secondTop = $(panels[1]).offset().top;
+
+	console.log(firstTop);
+	console.log(secondTop);
+
+	$(window).on("scroll", onScroll);
+
+	function onScroll() {
+	    if (scrollTimeout) {
+	        clearTimeout(scrollTimeout);
+	    }
+	    scrollTimeout = setTimeout(onScrollEnd, 500);
+	}
+
+	function onScrollEnd() {
+	    // Scrolling has stopped
+	    // Do stuff ...
+
+	   scrollTimeout = null;
+	}
+
 	console.log("Loading Nav.");
+	console.log(window.location.hash);
 	$('.navbar').onePageNav();
 
 	console.log("LOADED");
@@ -9,6 +44,56 @@ $(document).ready(function() {
 	$('li').click(function() {
 		$("li.active").removeClass("active");
 		$(this).addClass('active');
+		var href = $(this).children("a:first").attr('href');
+		window.location.hash = href;
+
+		console.log(href);
+
+	});
+
+	var viewportHeight = $(window).height();
+
+	function findViewportHeight() {
+		viewportHeight = $(window).height();
+	}
+
+	function checkLocation() {
+		currentLocation = $(window).scrollTop();
+
+		console.log(currentLocation);
+
+		if(currentLocation < secondTop) {
+			console.log("Adding class.");
+			navbar.addClass('hidden');
+
+		} else {
+
+			console.log("Removing class.");
+			navbar.removeClass('hidden');
+		}
+
+		console.log($(window).scrollTop());
+		/*if(window.location.hash == "#home") {
+			console.log("Adding class.");
+			//navbar.addClass('hidden');
+		} else {
+			console.log("Removing class.");
+			navbar.removeClass('hidden');
+		}*/
+
+	}
+	var panelTops = [];
+
+	$(window).on("resize", findViewportHeight);
+
+
+    var navbar = $('.navbar');
+
+	var links =  $('#nav li');
+	var panelIndex;
+
+	$(window).scroll(function () {
+		checkLocation();
 	});
 
 	$('.navbar a').click(function() {
@@ -18,7 +103,6 @@ $(document).ready(function() {
 	console.log("Now we're just checking something else...");
 
 	$('.nav a').on('click', function() {
-
 		if ($('.navbar-toggle').is(":visible")) {
 			$('.navbar-toggle').click();
 		}
